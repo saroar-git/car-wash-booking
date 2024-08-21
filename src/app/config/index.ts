59db563eb8dfd5969a3c364007085f1f70387904
saroar-git <info.saroarjahan@gmail.com>
2024-08-21 23:10:17 +0600

    config file added

---
+++ src/app/config/index.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.join(process.cwd(), '.env') });
+
+export default {
+  nodeEnv: process.env.NODE_ENV,
+  port: process.env.PORT,
+  databaseUrl: process.env.DATABASE_URL,
+  saltRound: process.env.SALT_ROUND,
+  jwtSecret: process.env.JWT_SECRET_KEY,
+};
